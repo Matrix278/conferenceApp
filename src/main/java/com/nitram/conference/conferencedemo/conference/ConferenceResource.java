8b35565ea658626da853c053c9d4919a0bbec76b
Martin Sidorov <35573364+Matrix278@users.noreply.github.com>
2019-09-30 12:41:48 +0300

    Add origin

--- src/main/java/com/nitram/conference/conferencedemo/conference/ConferenceResource.java
+++ src/main/java/com/nitram/conference/conferencedemo/conference/ConferenceResource.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200", "http://localhost:5000" })
 @RestController
 public class ConferenceResource {
 
